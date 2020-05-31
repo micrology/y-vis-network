4262ac16f08c4c7b3d62f5c389d0143b91a13c7f
Nigel Gilbert <n.gilbert@surrey.ac.uk>
2020-05-31 15:41:54 +0100

    Reformat source files

--- dist/y-vis-network.bundle.js
+++ dist/y-vis-network.bundle.js
@@ -55639,10 +55639,6 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
-
-
-
-
 /* 
 Remember to start the WS provider first:
 	npx y-websocket-server
@@ -55656,19 +55652,24 @@ window.addEventListener('load', () => {
 create a new shared document and start the WebSocket provider
  */
 const doc = new yjs__WEBPACK_IMPORTED_MODULE_0__["Doc"]();
-const wsProvider = new y_websocket__WEBPACK_IMPORTED_MODULE_1__["WebsocketProvider"]('ws://localhost:1234', 'y-vis-network-example6', doc);
-wsProvider.on('status', event => {
-	console.log(event.status) // logs "connected" or "disconnected"
-})
+const wsProvider = new y_websocket__WEBPACK_IMPORTED_MODULE_1__["WebsocketProvider"](
+	'ws://localhost:1234',
+	'y-vis-network-example6',
+	doc
+);
+wsProvider.on('status', (event) => {
+	console.log(event.status); // logs "connected" or "disconnected"
+});
 
 /* 
-create a yMap for the nodes and one for the edges (we need two because there is no 
-guarantee that the the ids of nodes will differ from the ids of edges 
- */
+create a yMap for the nodes and one for the edges (we need two because there is
+no guarantee that the the ids of nodes will differ from the ids of edges 
+*/
 const yNodesMap = doc.getMap('nodes');
 const yEdgesMap = doc.getMap('edges');
 
-const clientID = doc.clientID;  // used to identify nodes and edges created by this client
+// used to identify nodes and edges created by this client
+const clientID = doc.clientID;
 console.log('My client ID: ' + doc.clientID);
 
 var network = null;
@@ -55676,46 +55677,47 @@ const nodes = new vis_data__WEBPACK_IMPORTED_MODULE_3__["DataSet"]();
 const edges = new vis_data__WEBPACK_IMPORTED_MODULE_3__["DataSet"]();
 const data = {
 	nodes: nodes,
-	edges: edges
+	edges: edges,
 };
 
-/* 
-for convenience when debugging
- */
+//for convenience when debugging
 window.data = data;
 window.yNodesMap = yNodesMap;
 window.yEdgesMap = yEdgesMap;
 
 /* 
-nodes.on listens for when local nodes or edges are changed (added, updated or removed).
-If a local node is removed, the yMap is updated to broadcat to other clients that the node 
-has been deleted. If a local node is added or updated, that is also broadcast, with a 
-copy of the node, augmented with this client's ID, so that the originator can be identified.
-Nodes that are not originated locally are not broadcast (if they were, there would be a 
-feedback loop, with each client re-broadcasting everything it received)
- */
+nodes.on listens for when local nodes or edges are changed (added, updated or 
+removed). If a local node is removed, the yMap is updated to broadcat to 
+other clients that the node has been deleted. If a local node is added or 
+updated, that is also broadcast, with a copy of the node, augmented with this 
+client's ID, so that the originator can be identified.
+
+Nodes that are not originated locally are not broadcast (if they were, 
+there would be a feedback loop, with each client re-broadcasting everything 
+it received).
+*/
 
 nodes.on('*', (event, properties) => {
-	properties.items.forEach(id => {
+	properties.items.forEach((id) => {
 		if (event == 'remove') {
-			yNodesMap.delete(id.toString())
-		}
-		else {
+			yNodesMap.delete(id.toString());
+		} else {
 			let obj = nodes.get(id);
 			if (obj.clientID == undefined || obj.clientID == clientID) {
 				obj.clientID = clientID;
 				yNodesMap.set(id.toString(), obj);
 			}
 		}
-	})
+	});
 });
 
 /* 
-yNodesMap.observe listens for changes in the yMap, reciving a set of the keys that have
-had changed values.  If the change was to delete an entry, the corresponding node is
-removed from the local nodes dataSet. Otherwise, the local node dataSet is updated (which 
-includes adding a new node if it does not already exist locally).
- */
+yNodesMap.observe listens for changes in the yMap, receiving a set of the keys 
+that have had changed values.  If the change was to delete an entry, the 
+corresponding node is removed from the local nodes dataSet. Otherwise, the 
+local node dataSet is updated (which includes adding a new node if it does not 
+already exist locally).
+*/
 
 yNodesMap.observe((event, trans) => {
 	for (let key of event.keysChanged) {
@@ -55724,27 +55726,25 @@ yNodesMap.observe((event, trans) => {
 			if (obj.clientID != clientID) {
 				nodes.update(obj);
 			}
-		}
-		else nodes.remove(key);
+		} else nodes.remove(key);
 	}
 });
 
 /* 
 See comments above about nodes
- */
+*/
 edges.on('*', (event, properties) => {
-	properties.items.forEach(id => {
+	properties.items.forEach((id) => {
 		if (event == 'remove') {
-			yEdgesMap.delete(id.toString())
-		}
-		else {
+			yEdgesMap.delete(id.toString());
+		} else {
 			let obj = edges.get(id);
 			if (obj.clientID == undefined || obj.clientID == clientID) {
 				obj.clientID = clientID;
 				yEdgesMap.set(id.toString(), obj);
 			}
 		}
-	})
+	});
 });
 
 yEdgesMap.observe((event, trans) => {
@@ -55754,55 +55754,71 @@ yEdgesMap.observe((event, trans) => {
 			if (obj.clientID != clientID) {
 				edges.update(obj);
 			}
-		}
-		else edges.remove(key);
+		} else edges.remove(key);
 	}
 });
 
-function draw() {
 /* 
-draw() is unchanged from the standard example from vis-network, with the exception of the 
-statements 'data.clientID = undefined;' (added twice).
- */
+draw() is unchanged from the standard example from vis-network, with the 
+exception of the statements 'data.clientID = undefined;' (added twice).
+*/
+function draw() {
 	// create a network
 	var container = document.getElementById('mynetwork');
 	var options = {
 		manipulation: {
-			addNode: function(data, callback) {
+			addNode: function (data, callback) {
 				// filling in the popup DOM elements
-				document.getElementById('operation').innerHTML = "Add Node";
+				document.getElementById('operation').innerHTML = 'Add Node';
 				document.getElementById('node-id').value = data.id;
 				document.getElementById('node-label').value = data.label;
-				document.getElementById('saveButton').onclick = saveData.bind(this, data, callback);
-				document.getElementById('cancelButton').onclick = clearPopUp.bind();
-				document.getElementById('network-popUp').style.display = 'block';
+				document.getElementById('saveButton').onclick = saveData.bind(
+					this,
+					data,
+					callback
+				);
+				document.getElementById(
+					'cancelButton'
+				).onclick = clearPopUp.bind();
+				document.getElementById('network-popUp').style.display =
+					'block';
 			},
-			editNode: function(data, callback) {
+			editNode: function (data, callback) {
 				// filling in the popup DOM elements
-				document.getElementById('operation').innerHTML = "Edit Node";
+				document.getElementById('operation').innerHTML = 'Edit Node';
 				document.getElementById('node-id').value = data.id;
 				document.getElementById('node-label').value = data.label;
-				document.getElementById('saveButton').onclick = saveData.bind(this, data, callback);
-				document.getElementById('cancelButton').onclick = cancelEdit.bind(this, callback);
-				document.getElementById('network-popUp').style.display = 'block';
-				data.clientID = undefined; // allow this client to edit nodes created by other clients
+				document.getElementById('saveButton').onclick = saveData.bind(
+					this,
+					data,
+					callback
+				);
+				document.getElementById(
+					'cancelButton'
+				).onclick = cancelEdit.bind(this, callback);
+				document.getElementById('network-popUp').style.display =
+					'block';
+				// allow this client to edit nodes created by other clients
+				data.clientID = undefined;
 			},
-			addEdge: function(data, callback) {
+			addEdge: function (data, callback) {
 				if (data.from == data.to) {
-					var r = confirm("Do you want to connect the node to itself?");
+					var r = confirm(
+						'Do you want to connect the node to itself?'
+					);
 					if (r == true) {
 						callback(data);
 					}
-				}
-				else {
+				} else {
 					callback(data);
 				}
 			},
-			editEdge: function(data, callback) {
+			editEdge: function (data, callback) {
+				// allow this client to edit edges created by other clients
 				data.clientID = undefined;
-				callback(data); // allow this client to edit edges created by other clients
-			}
-		}
+				callback(data);
+			},
+		},
 	};
 
 	network = new vis_network_peer_esm_vis_network__WEBPACK_IMPORTED_MODULE_2__["Network"](container, data, options);
@@ -55825,21 +55841,19 @@ statements 'data.clientID = undefined;' (added twice).
 		callback(data);
 	}
 
-/* 
-activate a button to swith the provider on or off (demonstrates that
-the network can be changed ofline and then updates when back on line)
- */
-	const connectBtn = (document.getElementById('y-connect-btn'))
+	// activate a button to switch the provider on or off (demonstrates that
+	// the network can be changed ofline and then updates when back on line)
+
+	const connectBtn = document.getElementById('y-connect-btn');
 	connectBtn.addEventListener('click', () => {
 		if (wsProvider.shouldConnect) {
-			wsProvider.disconnect()
-			connectBtn.textContent = 'Connect'
+			wsProvider.disconnect();
+			connectBtn.textContent = 'Connect';
+		} else {
+			wsProvider.connect();
+			connectBtn.textContent = 'Disconnect';
 		}
-		else {
-			wsProvider.connect()
-			connectBtn.textContent = 'Disconnect'
-		}
-	})
+	});
 }
 
 function init() {
--- y-vis-network.js
+++ y-vis-network.js
@@ -5,20 +5,10 @@
 	January 2020
 */
 
-
-
 import * as Y from 'yjs';
-import {
-	WebsocketProvider
-}
-from 'y-websocket';
-
-import * as vis from "vis-network/peer/esm/vis-network";
-
-import {
-	DataSet
-}
-from "vis-data";
+import { WebsocketProvider } from 'y-websocket';
+import * as vis from 'vis-network/peer/esm/vis-network';
+import { DataSet } from 'vis-data';
 
 import 'vis-network/dist/vis-network.min.css';
 
@@ -35,19 +25,24 @@ window.addEventListener('load', () => {
 create a new shared document and start the WebSocket provider
  */
 const doc = new Y.Doc();
-const wsProvider = new WebsocketProvider('ws://localhost:1234', 'y-vis-network-example6', doc);
-wsProvider.on('status', event => {
-	console.log(event.status) // logs "connected" or "disconnected"
-})
+const wsProvider = new WebsocketProvider(
+	'ws://localhost:1234',
+	'y-vis-network-example6',
+	doc
+);
+wsProvider.on('status', (event) => {
+	console.log(event.status); // logs "connected" or "disconnected"
+});
 
 /* 
-create a yMap for the nodes and one for the edges (we need two because there is no 
-guarantee that the the ids of nodes will differ from the ids of edges 
- */
+create a yMap for the nodes and one for the edges (we need two because there is
+no guarantee that the the ids of nodes will differ from the ids of edges 
+*/
 const yNodesMap = doc.getMap('nodes');
 const yEdgesMap = doc.getMap('edges');
 
-const clientID = doc.clientID;  // used to identify nodes and edges created by this client
+// used to identify nodes and edges created by this client
+const clientID = doc.clientID;
 console.log('My client ID: ' + doc.clientID);
 
 var network = null;
@@ -55,46 +50,47 @@ const nodes = new DataSet();
 const edges = new DataSet();
 const data = {
 	nodes: nodes,
-	edges: edges
+	edges: edges,
 };
 
-/* 
-for convenience when debugging
- */
+//for convenience when debugging
 window.data = data;
 window.yNodesMap = yNodesMap;
 window.yEdgesMap = yEdgesMap;
 
 /* 
-nodes.on listens for when local nodes or edges are changed (added, updated or removed).
-If a local node is removed, the yMap is updated to broadcat to other clients that the node 
-has been deleted. If a local node is added or updated, that is also broadcast, with a 
-copy of the node, augmented with this client's ID, so that the originator can be identified.
-Nodes that are not originated locally are not broadcast (if they were, there would be a 
-feedback loop, with each client re-broadcasting everything it received)
- */
+nodes.on listens for when local nodes or edges are changed (added, updated or 
+removed). If a local node is removed, the yMap is updated to broadcat to 
+other clients that the node has been deleted. If a local node is added or 
+updated, that is also broadcast, with a copy of the node, augmented with this 
+client's ID, so that the originator can be identified.
+
+Nodes that are not originated locally are not broadcast (if they were, 
+there would be a feedback loop, with each client re-broadcasting everything 
+it received).
+*/
 
 nodes.on('*', (event, properties) => {
-	properties.items.forEach(id => {
+	properties.items.forEach((id) => {
 		if (event == 'remove') {
-			yNodesMap.delete(id.toString())
-		}
-		else {
+			yNodesMap.delete(id.toString());
+		} else {
 			let obj = nodes.get(id);
 			if (obj.clientID == undefined || obj.clientID == clientID) {
 				obj.clientID = clientID;
 				yNodesMap.set(id.toString(), obj);
 			}
 		}
-	})
+	});
 });
 
 /* 
-yNodesMap.observe listens for changes in the yMap, reciving a set of the keys that have
-had changed values.  If the change was to delete an entry, the corresponding node is
-removed from the local nodes dataSet. Otherwise, the local node dataSet is updated (which 
-includes adding a new node if it does not already exist locally).
- */
+yNodesMap.observe listens for changes in the yMap, receiving a set of the keys 
+that have had changed values.  If the change was to delete an entry, the 
+corresponding node is removed from the local nodes dataSet. Otherwise, the 
+local node dataSet is updated (which includes adding a new node if it does not 
+already exist locally).
+*/
 
 yNodesMap.observe((event, trans) => {
 	for (let key of event.keysChanged) {
@@ -103,27 +99,25 @@ yNodesMap.observe((event, trans) => {
 			if (obj.clientID != clientID) {
 				nodes.update(obj);
 			}
-		}
-		else nodes.remove(key);
+		} else nodes.remove(key);
 	}
 });
 
 /* 
 See comments above about nodes
- */
+*/
 edges.on('*', (event, properties) => {
-	properties.items.forEach(id => {
+	properties.items.forEach((id) => {
 		if (event == 'remove') {
-			yEdgesMap.delete(id.toString())
-		}
-		else {
+			yEdgesMap.delete(id.toString());
+		} else {
 			let obj = edges.get(id);
 			if (obj.clientID == undefined || obj.clientID == clientID) {
 				obj.clientID = clientID;
 				yEdgesMap.set(id.toString(), obj);
 			}
 		}
-	})
+	});
 });
 
 yEdgesMap.observe((event, trans) => {
@@ -133,55 +127,71 @@ yEdgesMap.observe((event, trans) => {
 			if (obj.clientID != clientID) {
 				edges.update(obj);
 			}
-		}
-		else edges.remove(key);
+		} else edges.remove(key);
 	}
 });
 
-function draw() {
 /* 
-draw() is unchanged from the standard example from vis-network, with the exception of the 
-statements 'data.clientID = undefined;' (added twice).
- */
+draw() is unchanged from the standard example from vis-network, with the 
+exception of the statements 'data.clientID = undefined;' (added twice).
+*/
+function draw() {
 	// create a network
 	var container = document.getElementById('mynetwork');
 	var options = {
 		manipulation: {
-			addNode: function(data, callback) {
+			addNode: function (data, callback) {
 				// filling in the popup DOM elements
-				document.getElementById('operation').innerHTML = "Add Node";
+				document.getElementById('operation').innerHTML = 'Add Node';
 				document.getElementById('node-id').value = data.id;
 				document.getElementById('node-label').value = data.label;
-				document.getElementById('saveButton').onclick = saveData.bind(this, data, callback);
-				document.getElementById('cancelButton').onclick = clearPopUp.bind();
-				document.getElementById('network-popUp').style.display = 'block';
+				document.getElementById('saveButton').onclick = saveData.bind(
+					this,
+					data,
+					callback
+				);
+				document.getElementById(
+					'cancelButton'
+				).onclick = clearPopUp.bind();
+				document.getElementById('network-popUp').style.display =
+					'block';
 			},
-			editNode: function(data, callback) {
+			editNode: function (data, callback) {
 				// filling in the popup DOM elements
-				document.getElementById('operation').innerHTML = "Edit Node";
+				document.getElementById('operation').innerHTML = 'Edit Node';
 				document.getElementById('node-id').value = data.id;
 				document.getElementById('node-label').value = data.label;
-				document.getElementById('saveButton').onclick = saveData.bind(this, data, callback);
-				document.getElementById('cancelButton').onclick = cancelEdit.bind(this, callback);
-				document.getElementById('network-popUp').style.display = 'block';
-				data.clientID = undefined; // allow this client to edit nodes created by other clients
+				document.getElementById('saveButton').onclick = saveData.bind(
+					this,
+					data,
+					callback
+				);
+				document.getElementById(
+					'cancelButton'
+				).onclick = cancelEdit.bind(this, callback);
+				document.getElementById('network-popUp').style.display =
+					'block';
+				// allow this client to edit nodes created by other clients
+				data.clientID = undefined;
 			},
-			addEdge: function(data, callback) {
+			addEdge: function (data, callback) {
 				if (data.from == data.to) {
-					var r = confirm("Do you want to connect the node to itself?");
+					var r = confirm(
+						'Do you want to connect the node to itself?'
+					);
 					if (r == true) {
 						callback(data);
 					}
-				}
-				else {
+				} else {
 					callback(data);
 				}
 			},
-			editEdge: function(data, callback) {
+			editEdge: function (data, callback) {
+				// allow this client to edit edges created by other clients
 				data.clientID = undefined;
-				callback(data); // allow this client to edit edges created by other clients
-			}
-		}
+				callback(data);
+			},
+		},
 	};
 
 	network = new vis.Network(container, data, options);
@@ -204,21 +214,19 @@ statements 'data.clientID = undefined;' (added twice).
 		callback(data);
 	}
 
-/* 
-activate a button to swith the provider on or off (demonstrates that
-the network can be changed ofline and then updates when back on line)
- */
-	const connectBtn = (document.getElementById('y-connect-btn'))
+	// activate a button to switch the provider on or off (demonstrates that
+	// the network can be changed ofline and then updates when back on line)
+
+	const connectBtn = document.getElementById('y-connect-btn');
 	connectBtn.addEventListener('click', () => {
 		if (wsProvider.shouldConnect) {
-			wsProvider.disconnect()
-			connectBtn.textContent = 'Connect'
+			wsProvider.disconnect();
+			connectBtn.textContent = 'Connect';
+		} else {
+			wsProvider.connect();
+			connectBtn.textContent = 'Disconnect';
 		}
-		else {
-			wsProvider.connect()
-			connectBtn.textContent = 'Disconnect'
-		}
-	})
+	});
 }
 
 function init() {
